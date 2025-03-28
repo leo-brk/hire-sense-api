1f7e4119b679e92104538765b57f3ca4aeeece23
Leotrim Vojvoda <leotrimvojvoda@gmail.com>
2025-03-28 20:08:44 +0100

    Job refactor

--- src/main/java/com/boreksolutions/hiresenseapi/core/job/JobMapper.java
+++ src/main/java/com/boreksolutions/hiresenseapi/core/job/JobMapper.java
@@ -1,16 +1,17 @@
 package com.boreksolutions.hiresenseapi.core.job;
 
+import com.boreksolutions.hiresenseapi.common.PageObject;
 import com.boreksolutions.hiresenseapi.core.job.dto.request.CreateJob;
 import com.boreksolutions.hiresenseapi.core.job.dto.response.JobDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.data.domain.Page;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface JobMapper {
 
-    @Mapping(target = "updatedById", source = "updatedBy.id")
     @Mapping(target = "industryId", source = "industry.id")
     @Mapping(target = "companyId", source = "company.id")
     @Mapping(target = "cityId", source = "city.id")
@@ -22,9 +23,15 @@ public interface JobMapper {
     @Mapping(target = "city", ignore = true)
     Job toEntity(CreateJob createJob);
 
-    @Mapping(target = "updatedBy", ignore = true)
     @Mapping(target = "industry", ignore = true)
     @Mapping(target = "company", ignore = true)
     @Mapping(target = "city", ignore = true)
     void updateEntity(CreateJob updateJob, @MappingTarget Job job);
+
+    @Mapping(target = "data", source = "content")
+    @Mapping(target = "totalSize", expression = "java(page.getTotalElements())")
+    @Mapping(target = "totalPages", expression = "java(page.getTotalPages())")
+    @Mapping(target = "size", expression = "java(page.getSize())")
+    @Mapping(target = "page", expression = "java(page.getNumber())")
+    PageObject<JobDto> pageToPageObject(Page<Job> page);
 }
--- src/main/java/com/boreksolutions/hiresenseapi/core/job/JobService.java
+++ src/main/java/com/boreksolutions/hiresenseapi/core/job/JobService.java
@@ -5,32 +5,17 @@ import com.boreksolutions.hiresenseapi.core.job.dto.request.CreateJob;
 import com.boreksolutions.hiresenseapi.core.job.dto.request.JobFilter;
 import com.boreksolutions.hiresenseapi.core.job.dto.response.JobDto;
 import jakarta.validation.Valid;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import java.util.List;
 
 public interface JobService {
 
-    JobDto createJob(@Valid CreateJob createJob);
+    Long createJob(@Valid CreateJob createJob);
 
     JobDto getJobById(Long id);
 
     JobDto updateJob(Long id, CreateJob updateJob);
 
-    void deleteJob(Long id);
-
-    List<JobDto> getAllJobs();
-
-    List<JobDto> getJobsByCompany(Long companyId);
-
-    List<JobDto> getJobsByCity(Long cityId);
-
-    List<JobDto> getJobsByIndustry(Long industryId);
-
-    List<JobDto> getJobsByTitle(String title);
-
-    Page<JobDto> filterJobs(JobFilter filter, Pageable pageable);
+    void delete(Long id);
 
     PageObject<JobDto> filter(JobFilter jobFilter, Pageable pageable);
 }
