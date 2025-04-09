1e59c3164ba666517ee5bf93b9dee6eb99a6d113
LeotrimVojvoda <leotrimvojvoda@gmail.com>
2025-04-09 09:54:07 +0200

    Performance updates

--- src/main/java/com/boreksolutions/hiresenseapi/config/batch/entrypoint/CsvUploadService.java
+++ src/main/java/com/boreksolutions/hiresenseapi/config/batch/entrypoint/CsvUploadService.java
@@ -1,12 +1,18 @@
 package com.boreksolutions.hiresenseapi.config.batch.entrypoint;
 
+import com.boreksolutions.hiresenseapi.config.batch.cache.BatchCacheService;
+import com.boreksolutions.hiresenseapi.config.exceptions.models.BadRequestException;
+import com.boreksolutions.hiresenseapi.core.job.JobEntity;
+import com.boreksolutions.hiresenseapi.core.job.JobEntityRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -19,6 +25,8 @@ public class CsvUploadService {
 
     private final JobLauncher jobLauncher;
     private final Job job;
+    private final BatchCacheService batchCacheService;
+    private final JobEntityRepository jobRepository;
 
     public void receiveFile(MultipartFile file) {
         validateFile(file);
@@ -30,6 +38,14 @@ public class CsvUploadService {
         try {
             String uploadDir = new File("uploads").getAbsolutePath();
             File targetFile = new File(uploadDir, "data.csv");
+
+            if (!targetFile.exists()) {
+                if (targetFile.mkdirs())
+                    log.info("Directory created: " + uploadDir);
+                 else
+                    throw new RuntimeException("Failed to create directory: " + uploadDir);
+            }
+
             file.transferTo(targetFile);
             log.info("Uploaded file to: " + targetFile.getAbsolutePath());
             System.out.println("Saved to: " + targetFile.getAbsolutePath());
@@ -40,13 +56,13 @@ public class CsvUploadService {
 
     private void validateFile(MultipartFile file) {
         if (file.isEmpty())
-            throw new IllegalArgumentException("File is empty");
+            throw new BadRequestException("File is empty");
 
         if (!file.getOriginalFilename().toLowerCase().endsWith(".csv"))
-            throw new IllegalArgumentException("Invalid file type, only CSV files are allowed");
+            throw new BadRequestException("Invalid file type, only CSV files are allowed");
 
         if (!"text/csv".equalsIgnoreCase(file.getContentType()))
-            throw new IllegalArgumentException("Invalid content type, expected text/csv");
+            throw new BadRequestException("Invalid content type, expected text/csv");
     }
 
 
@@ -61,5 +77,12 @@ public class CsvUploadService {
             log.error("Failed to start CSV import job", e);
             throw new RuntimeException("Job launch failed", e);
         }
+    }
+
+    @Transactional
+    public void clearData() {
+        batchCacheService.clearCache();
+        jobRepository.clearData();
+
     }
 }
